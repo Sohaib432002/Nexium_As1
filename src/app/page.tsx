ab60772fc59249f21d22f2c669d2d30a5524b737
sohaib <sohaibmaqsoodsohaibmaqsood@gmail.com>
2025-07-05 00:27:10 +0500

    uplodede

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
-import Image from "next/image";
 import Navbar from "@/Components/Navbar";
 import Header from "@/Components/Header";
 import App from "@/Components/App";
 import About from "@/Components/About";
+
 export default function Home() {
-  
   return (
     <>
-    <Navbar />
-    <Header />
-    <App/>
-    <About/>
+      <Navbar />
+      <Header />
+      <App />
+      <About />
     </>
   );
 }
